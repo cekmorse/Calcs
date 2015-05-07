278079a22a1ae808585c6282a3ab26745ef53d21
Keith Morse <keith.morse@cekmorse@gmail.com>
2015-05-07 08:42:33 -0600

    Remove test R script and archived material

--- R/SubScripts/Archive/TS plots 2.R
+++
@@ -1,6 +0,0 @@
-par(mar=c(5.1,4.4,1.1,1.1))
-plot(xx$ti, xx$mean, type="n", ylim=ylimit, xlab="Time", ylab=ylabel, las=1)
-for(i in 1:length(polys)){polygon(polys[[i]]$x, polys[[i]]$y, col = rgb(0,0,1,0.25), border = rgb(0,0,1,0.25))}
-#lines(ev$ti, e, col="red", lty=2)
-lines(xx$ti, xx$mean, lwd=1, col="black")
-abline(h=0, lty=2)
--- R/SubScripts/Archive/panelpearson.R
+++
@@ -1,6 +0,0 @@
-###  FUNCTIONS  ###
-panel.pearson <- function(x, y, ...){
-  horizontal <- (par("usr")[1] + par("usr")[2]) / 2;
-  vertical <- (par("usr")[3] + par("usr")[4]) / 2;
-  text(horizontal, vertical, format(cor(x,y, use="complete.obs"), digits=2), cex=1)
-}
